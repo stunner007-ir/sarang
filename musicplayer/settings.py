3e47b4b073c5400175d04ae57cadd6d566378e28
Ishu Raj <100108719+stunner007-ir@users.noreply.github.com>
2024-01-14 13:25:46 +0530

    Update settings.py

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rbh8%u_$en!zodi6j_ual3azei=2(@yd1zrl7n0kz^t!xe$t2o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
